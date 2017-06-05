96ed832e2846407aeca84023237f9321217a0647
fma0043 <fma0043@alu.ubu.es>
2017-06-05 14:15:59 +0200

    Creación del constructor en Club.

--- Practica4_2C/src/practica4_2C/Club.java
+++ Practica4_2C/src/practica4_2C/Club.java
@@ -2,7 +2,28 @@ package practica4_2C;
 
 import java.util.ArrayList;
 
+/**
+ * Club.
+ * 
+ * @author Félix Movilla Alonso.
+ */
 public class Club {
+	
+	private int pelotasActuales;
+	private int palosActuales;
+	private boolean acceso;
+	private static Club club;
+	static ArrayList<Jugador> jugadores = new ArrayList<Jugador>();
+
+	/**
+	 * Constructor en el que inicializamos las variables.
+	 */
+	private Club(int numPelotasIniciales, int numPalosIniciales) {
+		this.pelotasActuales = numPelotasIniciales;
+		this.palosActuales = numPalosIniciales;
+		this.acceso = false;
+	}
+
 
 	public void reservar(int pelotas, int palos) {
 		// TODO Auto-generated method stub
