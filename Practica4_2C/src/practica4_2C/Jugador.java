a9b55db135c7eb6bc21279b96cc767a79dd57c8d
fma0043 <fma0043@alu.ubu.es>
2017-06-05 13:58:39 +0200

    Constructor de la clase Jugador.

--- Practica4_2C/src/practica4_2C/Jugador.java
+++ Practica4_2C/src/practica4_2C/Jugador.java
@@ -1,5 +1,51 @@
 package practica4_2C;
 
-public class Jugador {
 
+import java.lang.Runnable;
+
+/**
+ * Clase Hilo, que implementa la interfaz Runnable.
+ * 
+ * @author Félix Movilla Alonso.
+ * 
+ */
+public class Jugador implements Runnable {
+
+	private int vueltas, pelotasActuales, palosActuales;
+	String ident;
+	private Club club;
+	private boolean experto;
+
+	/**
+	 * Constructor de la clase.
+	 * 
+	 * @param int
+	 *            id.
+	 * @param boolean
+	 *            experto.
+	 * @param int
+	 *            numVueltas.
+	 * @param Club
+	 *            elClub.
+	 */
+	public Jugador(int id, boolean experto, int numVueltas, Club elClub) {
+		ident = "";
+		ident += id;
+		this.vueltas = numVueltas;
+		this.club = elClub;
+		this.palosActuales = 0;
+		this.pelotasActuales = 0;
+		this.experto = experto;
+		if (experto) {
+			ident += "+";
+		} else {
+			ident += "-";
+		}
+	}
+
+	@Override
+	public void run() {
+		
+		
+	}
 }
